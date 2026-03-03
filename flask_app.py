b312fd533f708f9312ad6c110f59b1084cc41817
SmallWaterBucket <humanhumanovic@gmail.com>
2026-03-03 21:27:41 +0100

    Maybe fixed smth

--- flask_app.py
+++ flask_app.py
@@ -308,7 +308,7 @@ def new_scrape():
         date = day.find_all("div", class_ = "jidelnicekTop semibold")[0].get("id").split("-")[1:]
         date = ".".join(date)
         date = datetime.datetime.strptime(date, "%Y.%m.%d")
-        disabled = date - today <= 2
+        disabled = (date - today).days <= 2
         
         foods = []
 
