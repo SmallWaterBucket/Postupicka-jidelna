22b070125d4d1d6ffd2d55c469bb612961998a53
SmallWaterBucket <humanhumanovic@gmail.com>
2026-03-04 11:20:52 +0100

    Comment out new_scrape() return in debug route
    Comment out the return statement for new_scrape() in the debug route.

--- flask_app.py
+++ flask_app.py
@@ -235,7 +235,7 @@ def get_new_food(food_id):
 
 @app.route('/debug')
 def debug():
-    return str(new_scrape())
+    #return str(new_scrape())
     db = get_db()
     mycursor = db.cursor()
     data = new_scrape()
