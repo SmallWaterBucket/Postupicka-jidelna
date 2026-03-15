1fa47b17be860fc3008c041d5ffb850fb41c0e31
SmallWaterBucket <humanhumanovic@gmail.com>
2026-03-15 15:51:28 +0100

    more debugging

--- flask_app.py
+++ flask_app.py
@@ -343,8 +343,8 @@ def new_scrape(username, password):
 
             chosen_food_date = datetime.datetime.strftime(date, "%Y-%m-%d")
             chosen_food_dates.append(chosen_food_date)
-        return str(chosen_food_dates)
         chosen_foods_str = get_orderd_food(username, password, chosen_food_dates)
+        return chosen_foods_str
         chosen_foods = chosen_foods_str.split(" ")
     
 
