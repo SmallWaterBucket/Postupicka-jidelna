3ad25b2825e9375896ee00663914d275325e5c9c
SmallWaterBucket <humanhumanovic@gmail.com>
2026-04-01 20:21:42 +0200

    Maybe speeded up the loading for /foods

--- flask_app.py
+++ flask_app.py
@@ -426,7 +426,7 @@ def all_foods():
     answer = mycursor.fetchall()
     ret = []
     for item in answer:
-        food_item = item[1], item[0], get_image(item[1]), item[3]  # food, food_id,image,rating
+        food_item = item[1], item[0], item[2], item[3]  # food, food_id,image,rating
         ret.append(food_item)
 
     return render_template("foods.html", foods=ret)
