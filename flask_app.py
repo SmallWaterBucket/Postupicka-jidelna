349fb1e1188f826fec6f4c61a6e6b2088f1f97a0
SmallWaterBucket <humanhumanovic@gmail.com>
2026-05-03 15:29:37 +0200

    Changed some code to make it work

--- flask_app.py
+++ flask_app.py
@@ -174,13 +174,18 @@ def get_food(food_id):
     user_rating = ""
     if request.method == "POST":
         new_rating = request.form.get("rating")
-
-        cursor.execute("SELECT rating FROM Ratings WHERE foodid = %s AND username = %s;", (id, session.get("user")))
-        user_rating = cursor.fetchone()
+        if session.get("user") and session.get("password"):
+            cursor.execute("SELECT rating FROM Ratings WHERE foodid = %s AND username = %s;", (id, session.get("user")))
+            user_rating = cursor.fetchone()
 
 
         if not user_rating:
-            cursor.execute("insert into Ratings (foodid,rating,username) values (%s,%s,%s)", (id,new_rating,session.get("user")))
+            if not session.get("user"):
+                username = "Anonymous"
+            else:
+                username = session.get("user")
+
+            cursor.execute("insert into Ratings (foodid,rating,username) values (%s,%s,%s)", (id,new_rating,username))
             db.commit()
         cursor.execute("SELECT AVG(rating) AS AveragePrice FROM Ratings where foodid = %s;",(id,))
         ret = cursor.fetchall()
