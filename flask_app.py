f1c9b7dfe97b6793814be0a7be0df94874e791c7
SmallWaterBucket <humanhumanovic@gmail.com>
2026-06-25 15:48:28 +0200

    Changed port

--- flask_app.py
+++ flask_app.py
@@ -681,4 +681,4 @@ def order_food(username, password, date, food_id):
     page = requests.get(f"https://sparkling-sun-0a6e.humanhumanovic.workers.dev/?url=http://152.70.41.16.nip.io:8080/order.exe/{username},{password},{date},{food_id}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8080)
