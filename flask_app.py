6e39a333f7028fd20a19c731824c9265289a19cd
SmallWaterBucket <196561045+SmallWaterBucket@users.noreply.github.com>
2026-08-29 21:25:11 +0200

    Hopefully rewrote the function for Strava to work

--- flask_app.py
+++ flask_app.py
@@ -13,7 +13,15 @@ from flask import json
 from werkzeug.exceptions import HTTPException
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
-from strava_cz import StravaCZ, MealType, OrderType
+from strava_cz import (
+    StravaCZ,
+    AuthenticationError,
+    InvalidMealTypeError,
+    InsufficientBalanceError,
+    StravaAPIError,
+    MealType,
+    OrderType
+)
 
 
 #TODO:
@@ -1123,64 +1131,111 @@ def iCanteen_scrape(): #day,day_str,foods,chosen_food
 @app.route("/strava/login/<username>,<password>")
 def Strava_try_login(username, password):
     canteen_number = canteen_id_to_url(get_canteen_id())
-    page = requests.get(f"https://sparkling-sun-0a6e.humanhumanovic.workers.dev/?url=http://jidelna.qzz.io/strava/login/{username},{password},{canteen_number}")
-    return page.text
+    try:
+        strava_login_internal(username,password,canteen_number)
+    except AuthenticationError as e:
+        return "0"
+    return "1"
 
 @app.route("/strava/kredit/<username>,<password>")
 def Strava_kredit(username, password):
     canteen_number = canteen_id_to_url(get_canteen_id())
-    page = requests.get(f"https://sparkling-sun-0a6e.humanhumanovic.workers.dev/?url=http://jidelna.qzz.io/strava/kredit/{username},{password},{canteen_number}")
-    return page.text
+    strava = strava_login_internal(username, password, canteen_number)
+    return str(strava.user.balance)
 
-@app.route("/strava/fixed_scrape")
-def Strava_fixed_scrape():
-    canteen_number = canteen_id_to_url(get_canteen_id())
-    url = f"https://sparkling-sun-0a6e.humanhumanovic.workers.dev/?url=jidelna.qzz.io/strava/scrape/{canteen_number}"
-
-@app.route("/strava/scrape")
+@app.route("/strava/scrape/")
 def Strava_scrape():
     canteen_number = canteen_id_to_url(get_canteen_id())
-    url = f"https://sparkling-sun-0a6e.humanhumanovic.workers.dev/?url=https://www.strava.cz/strava5/Jidelnicky/XML?zarizeni={canteen_number}"
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-    except requests.RequestException:
-        return -1
-
-    root = ET.fromstring(response.content)
+    url = "https://app.strava.cz/api/jidelnickyPage"
     
-    data = []
+    headers = {
+        "Accept": "*/*",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Content-Type": "text/plain;charset=UTF-8",
+        "Origin": "https://app.strava.cz",
+        "Referer": f"https://app.strava.cz/jidelnicky?jidelna={canteen_number}",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/152.0.0.0 Safari/537.36",
+    }
+    
+    payload = {
+        "cislo": str(canteen_number),
+        "lang": "CZ"
+    }
+    
+    response = requests.post(
+        url,
+        headers=headers,
+        data=json.dumps(payload),
+        timeout=15
+    )
+    
+    response.raise_for_status()
+    
+    data = response.json()
+    
+    days = []
+    
+    for key, meals in data["meals"].items():
+        if not key.startswith("table"):
+            continue
+        
+        if not meals:
+            continue
+        
+        date = meals[0]["datum"]
+    
+        mymeals = []
+        print(date)
+        for meal in meals:
+            if meal["druh"] != 'P':
+                print(f"meal name: {meal['nazev']}")
+                mymeals.append(meal["nazev"])
+    
+        days.append((date,mymeals))
+    return days
 
-    for day in root.findall("den"):
-        foods = []
-        date_str = day.get("datum")
-        date = datetime.datetime.strptime(date_str, "%d-%m-%Y")
-        date_out = f"{datetime.datetime.strftime(date, "%d.%m.%Y")}"
-        for food in day.findall("jidlo"):
-            name = food.get("nazev")
-            type = food.get("druh")
-            if type != "Polévka " and type != "Doplněk ":
-                foods.append(name)
-        data.append((date_out,foods))
-    return data
+@app.route("/strava/get_ordered_food/<username>,<password>,<dates>") # lets hope this works :crying: :hope:
+def Strava_get_ordered_food(username, password, dates):
+    canteen_number = canteen_id_to_url(get_canteen_id())
+    strava = strava_login_internal(username, password, canteen_number)
+    ordered_indexes = []
+    for i in range(len(dates)):
+        ordered_indexes.append('')
 
-@app.route("/strava/ordered_foods/<username>,<password>,<dates>,<canteen_id>") # lets hope this works :crying: :hope:
-def Strava_get_ordered_food(username, password, dates, canteen_id = None):
-    if canteen_id is None:
-        canteen_id = get_canteen_id()
-    dates = ".".join(dates)
-    canteen_id = canteen_id_to_url(canteen_id)
-    page = requests.get(f"https://sparkling-sun-0a6e.humanhumanovic.workers.dev/?url=http://jidelna.qzz.io/strava/get_ordered_foods/{username},{password},{dates},{canteen_id}")
-    return page.text
+        menu = strava.menu.get_by_date(dates[i])
+
+        if menu:
+            for meal_index in range(len(menu["meals"])):
+                meal = menu["meals"][meal_index]
+                if meal["ordered"]:
+                    ordered_indexes[-1] = meal_index
+    if len(ordered_indexes) < 1:
+        return ""
+    return ';'.join(ordered_indexes) + ";"
 
 @app.route("/strava/order/<username>,<password>,<date>,<food>")
 def Strava_order_food(username, password, date, food):
     canteen_number = canteen_id_to_url(get_canteen_id())
-    page = requests.get(f"https://sparkling-sun-0a6e.humanhumanovic.workers.dev/?url=http://jidelna.qzz.io/strava/order/{username},{password},{date},{food},{canteen_number}")
-    return page.text
+    strava = strava_login_internal(username, password, canteen_number)
+    food = int(food)
+    menu = strava.menu.get_by_date(date)
+    if not menu:
+        return "Date not found"
+    id = menu['meals'][food]['id']
+    strava.menu.order_meals(id)
+    ordered = strava.menu.is_ordered(id)
+    return str(ordered)
 
 
+
+def strava_login_internal(username, password, canteen_number):
+    strava = StravaCZ(
+            username=username,
+            password=password,
+            canteen_number = canteen_number
+        )
+    return strava
 
 
 
